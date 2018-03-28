eb1d747eea113dbb179b5d7819fe95b838bbe72c
haripermadi <harinurpermadi@gmail.com>
2018-03-28 10:44:06 +0700

    populate

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,18 +1,28 @@
 const Transaction = require('../models/Transaction')
 module.exports = {
   all: function(req, res) {
-    Transaction.find(function (err, transactions) {
-      if (err) {
-        res.send({err: err})
-      }
-      res.send(transactions)
+    Transaction.find()
+    .populate('booklist')
+    .exec()
+    .then(data=>{
+      res.status(200).json({
+        message: 'success',
+        data
+      })
     })
+    // Transaction.find(function (err, transactions) {
+    //   if (err) {
+    //     res.send({err: err})
+    //   }
+    //   res.send(transactions)
+    // })
   },
   create: function(req, res) {
     let input = {
       memberid: req.body.memberid,
       days: req.body.days,
       price: req.body.price,
+      booklist: req.body.booklist
     }
     var transaction = new Transaction(input);
     transaction.save().then(data=>{
